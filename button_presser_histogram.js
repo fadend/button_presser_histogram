2ea611a05ed72232420801787976f179e3f1d177
David Faden (revfad.com) <dfaden@gmail.com>
2025-07-24 16:42:48 -0700

    Switch to class-based design
    Avoids globals... a little cleaner?

--- button_presser_histogram.js
+++ button_presser_histogram.js
@@ -1,75 +1,92 @@
-const pressCountDisplay = document.getElementById("button-press-count");
-const timerDisplay = document.getElementById("milliseconds-timer");
-const extraStatsDiv = document.getElementById("extra-stats");
-const lastTimeDisplay = document.getElementById("last-time");
-const meanTimeDisplay = document.getElementById("mean-time");
-const durationDisplay = document.getElementById("duration");
+class ButtonPresser {
+  constructor(parentElem) {
+    this.pressCountDisplay = parentElem.querySelector(".button-press-count");
+    this.timerDisplay = parentElem.querySelector(".milliseconds-timer");
+    this.extraStatsDiv = parentElem.querySelector(".extra-stats");
+    this.lastTimeDisplay = parentElem.querySelector(".last-time");
+    this.meanTimeDisplay = parentElem.querySelector(".mean-time");
+    this.durationDisplay = parentElem.querySelector(".duration");
+    this.histogramElem = parentElem.querySelector(".button-press-histogram");
+    this.timeseriesPlotElem = parentElem.querySelector(
+      ".button-press-timeseries",
+    );
+    this.startTime = -1;
+    this.lastPressTime = -1;
+    this.pressTimes = [];
+    this.perSecondCount = [0];
+    this.seconds = [0];
 
-let startTime = -1;
-let lastPressTime = -1;
-const pressTimes = [];
-const perSecondCount = [0];
-const seconds = [0];
+    // Add event handling.
+    parentElem
+      .querySelector(".button")
+      .addEventListener("click", () => this.handleClick());
 
-function extendSecondsToMatchDuration(duration) {
-  const secondsOffset = Math.floor(duration / 1000);
-  if (seconds[seconds.length - 1] != secondsOffset) {
-    seconds.push(secondsOffset);
-    perSecondCount.push(0);
+    setInterval(() => {
+      this.timerDisplay.textContent = +new Date() - this.lastPressTime;
+    }, 50);
+
+    setInterval(() => this.handleTick(), 500);
+  }
+
+  handleClick() {
+    const now = +new Date();
+    if (this.lastPressTime > 0) {
+      const elapsed = now - this.lastPressTime;
+      this.lastTimeDisplay.textContent = elapsed;
+      this.pressTimes.push(elapsed);
+    }
+    if (this.startTime < 0) {
+      this.startTime = now;
+    }
+    this.lastPressTime = now;
+    const numPresses = 1 + this.pressTimes.length;
+    this.pressCountDisplay.textContent = numPresses;
+    const totalDuration = now - this.startTime;
+    this.meanTimeDisplay.textContent = (totalDuration / numPresses).toFixed(1);
+    // If we have data, show some extra stats.
+    if (this.pressTimes.length) {
+      this.extraStatsDiv.style.display = "";
+    }
+    this.extendSecondsToMatchDuration(totalDuration);
+    this.perSecondCount[this.perSecondCount.length - 1]++;
+    Plotly.newPlot(
+      this.histogramElem,
+      [{ x: this.pressTimes, type: "histogram" }],
+      {
+        autosize: false,
+        width: 800,
+        xaxis: { title: { text: "Button press time (milliseconds)" } },
+        yaxis: { title: { text: "Count per bucket" } },
+      },
+    );
+  }
+
+  handleTick() {
+    if (this.startTime < 0) {
+      return;
+    }
+    const duration = +new Date() - this.startTime;
+    this.durationDisplay.textContent = Math.floor(duration / 1000).toFixed(0);
+    this.extendSecondsToMatchDuration(duration);
+    Plotly.newPlot(
+      this.timeseriesPlotElem,
+      [{ x: this.seconds, y: this.perSecondCount, type: "scatter" }],
+      {
+        autosize: false,
+        width: 800,
+        xaxis: { title: { text: "Seconds since start" } },
+        yaxis: { title: { text: "Count per second" } },
+      },
+    );
+  }
+
+  extendSecondsToMatchDuration(duration) {
+    const secondsOffset = Math.floor(duration / 1000);
+    if (this.seconds[this.seconds.length - 1] != secondsOffset) {
+      this.seconds.push(secondsOffset);
+      this.perSecondCount.push(0);
+    }
   }
 }
 
-document.getElementById("button").addEventListener("click", function () {
-  const now = +new Date();
-  if (lastPressTime > 0) {
-    const elapsed = now - lastPressTime;
-    lastTimeDisplay.textContent = elapsed;
-    pressTimes.push(elapsed);
-  }
-  if (startTime < 0) {
-    startTime = now;
-  }
-  lastPressTime = now;
-  const numPresses = 1 + pressTimes.length;
-  pressCountDisplay.textContent = numPresses;
-  const totalDuration = now - startTime;
-  meanTimeDisplay.textContent = (totalDuration / numPresses).toFixed(1);
-  if (pressTimes.length) {
-    extraStatsDiv.style.display = "";
-  }
-  extendSecondsToMatchDuration(totalDuration);
-  perSecondCount[perSecondCount.length - 1]++;
-  Plotly.newPlot(
-    "button-press-histogram",
-    [{ x: pressTimes, type: "histogram" }],
-    {
-      autosize: false,
-      width: 800,
-      xaxis: { title: { text: "Button press time (milliseconds)" } },
-      yaxis: { title: { text: "Count per bucket" } },
-    },
-  );
-});
-
-setInterval(() => {
-  timerDisplay.textContent = +new Date() - lastPressTime;
-}, 50);
-
-setInterval(() => {
-  if (startTime < 0) {
-    return;
-  }
-  const duration = +new Date() - startTime;
-  durationDisplay.textContent = Math.floor(duration / 1000).toFixed(0);
-  extendSecondsToMatchDuration(duration);
-  Plotly.newPlot(
-    "button-press-timeseries",
-    [{ x: seconds, y: perSecondCount, type: "scatter" }],
-    {
-      autosize: false,
-      width: 800,
-      xaxis: { title: { text: "Seconds since start" } },
-      yaxis: { title: { text: "Count per second" } },
-    },
-  );
-}, 500);
+new ButtonPresser(document.body);
